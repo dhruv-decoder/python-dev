cbf66234e8bd26602acb1677cc1c66904f161958
dhruv-decoder <121481578+dhruv-decoder@users.noreply.github.com>
2023-11-01 01:22:22 +0530

    python start

---
+++ main.py
@@ -0,0 +1,5 @@
+# Write your code below this line 👇
+print("hello "+input("name:"))
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
